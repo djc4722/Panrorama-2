c32d729214bb4d7783d00e6b53159f4b07ac922f
djc4722 <31022447+djc4722@users.noreply.github.com>
2017-10-13 14:06:51 -0400

    1

--- marzi/data.js
+++ marzi/data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-scene-1",
-      "name": "Scene 1",
+      "id": "0-vr-test_location_4",
+      "name": "VR TEST_Location_4",
       "levels": [
         {
           "tileSize": 256,
@@ -16,26 +16,298 @@ var APP_DATA = {
         {
           "tileSize": 512,
           "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
         }
       ],
-      "faceSize": 1024,
+      "faceSize": 2048,
       "initialViewParameters": {
-        "pitch": 0,
-        "yaw": 0,
+        "yaw": -2.654918973496663,
+        "pitch": -0.011344269046970723,
         "fov": 1.5707963267948966
       },
-      "linkHotspots": [],
-      "infoHotspots": [
+      "linkHotspots": [
         {
-          "yaw": -1.8023956310932725,
-          "pitch": -0.16282520369832554,
-          "title": "Title",
-          "text": "Text"
+          "yaw": -2.7152250271261416,
+          "pitch": 0.019435569354021354,
+          "rotation": 0,
+          "target": "1-vr-test_location_2"
+        },
+        {
+          "yaw": -1.9457503383751558,
+          "pitch": 0.04626750187779116,
+          "rotation": 0,
+          "target": "3-vr-test_location_5"
+        },
+        {
+          "yaw": -1.006951059037636,
+          "pitch": -0.021773831574449787,
+          "rotation": 0,
+          "target": "4-vr-test_location_6"
+        },
+        {
+          "yaw": 2.6718727101643367,
+          "pitch": 0.04599085551650006,
+          "rotation": 0,
+          "target": "5-vr-test_location_1"
         }
-      ]
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-vr-test_location_2",
+      "name": "VR TEST_Location_2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.0604695455571687,
+        "pitch": -0.0017087114589386942,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5783308852461406,
+          "pitch": 0.1073756929822487,
+          "rotation": 0,
+          "target": "2-vr-test_location_3"
+        },
+        {
+          "yaw": 1.842305956982071,
+          "pitch": -0.006959561864837482,
+          "rotation": 0,
+          "target": "4-vr-test_location_6"
+        },
+        {
+          "yaw": -3.0749946056199775,
+          "pitch": 0.0765181986110619,
+          "rotation": 0,
+          "target": "5-vr-test_location_1"
+        },
+        {
+          "yaw": 2.292611806089445,
+          "pitch": 0.05140569597071476,
+          "rotation": 0,
+          "target": "0-vr-test_location_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-vr-test_location_3",
+      "name": "VR TEST_Location_3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.3471520912046806,
+        "pitch": 0.11312925242537375,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9800088179007034,
+          "pitch": 0.030024747910145422,
+          "rotation": 0,
+          "target": "1-vr-test_location_2"
+        },
+        {
+          "yaw": -1.200037641462611,
+          "pitch": 0.035657096930890475,
+          "rotation": 0,
+          "target": "5-vr-test_location_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-vr-test_location_5",
+      "name": "VR TEST_Location_5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -2.2395268472494507,
+        "pitch": -0.04347460661066904,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5283621519906898,
+          "pitch": 0.0199565999706941,
+          "rotation": 0,
+          "target": "1-vr-test_location_2"
+        },
+        {
+          "yaw": -3.0775239756761934,
+          "pitch": 0.024926676913013424,
+          "rotation": 0,
+          "target": "0-vr-test_location_4"
+        },
+        {
+          "yaw": 2.168032264378435,
+          "pitch": -0.027341391894575295,
+          "rotation": 0,
+          "target": "4-vr-test_location_6"
+        },
+        {
+          "yaw": -2.254121067377998,
+          "pitch": 0.008209179659507626,
+          "rotation": 0,
+          "target": "5-vr-test_location_1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-vr-test_location_6",
+      "name": "VR TEST_Location_6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.770128321365835,
+        "pitch": -0.024174848411643524,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.3216620993950237,
+          "pitch": 0.018003989134953713,
+          "rotation": 0,
+          "target": "3-vr-test_location_5"
+        },
+        {
+          "yaw": -1.5864000316340459,
+          "pitch": 0.060973118697711826,
+          "rotation": 0,
+          "target": "0-vr-test_location_4"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-vr-test_location_1",
+      "name": "VR TEST_Location_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.8618029717777667,
+        "pitch": -0.026623113925046127,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.9590593593771422,
+          "pitch": 0.07212043422144987,
+          "rotation": 0,
+          "target": "1-vr-test_location_2"
+        },
+        {
+          "yaw": -2.5641417979394294,
+          "pitch": 0.040734524982386944,
+          "rotation": 0,
+          "target": "0-vr-test_location_4"
+        },
+        {
+          "yaw": 0.840158678808951,
+          "pitch": 0.09114791109360709,
+          "rotation": 0,
+          "target": "2-vr-test_location_3"
+        }
+      ],
+      "infoHotspots": []
     }
   ],
-  "name": "Park Branck",
+  "name": "SMNC",
   "settings": {
     "mouseViewMode": "drag",
     "autorotateEnabled": false,
